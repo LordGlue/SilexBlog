a01f706c6db54390a438a4120188448967efd2a7
Glue <lordglue@gmail.com>
2014-04-14 23:45:45 +0700

    Reformat code

--- app/app.php
+++ app/app.php
@@ -8,22 +8,30 @@
 require_once __DIR__ . '/bootstrap.php';
 
 /** * Before action */
-$app->before(function () use ($app) {
-	// Registering Twig
-	$app->register(new \Silex\Provider\TwigServiceProvider(), array(
-		'twig.path' => __DIR__ . '/../www/views',
-	));
-	/** @var $twig \Twig_Environment */
-	$twig = $app['twig'];
-	$twig->addGlobal('layout', $twig->loadTemplate('layout.twig'));
-});
+$app->before(
+	function () use ($app) {
+		// Registering Twig
+		$app->register(
+			new \Silex\Provider\TwigServiceProvider(),
+			array(
+				'twig.path' => __DIR__ . '/../www/views',
+			)
+		);
+		/** @var $twig \Twig_Environment */
+		$twig = $app['twig'];
+		$twig->addGlobal('layout', $twig->loadTemplate('layout.twig'));
+	}
+);
 
 
-$app->get('/', function () use ($app) {
-	/** @var $twig \Twig_Environment */
-	$twig = $app['twig'];
+$app->get(
+	'/',
+	function () use ($app) {
+		/** @var $twig \Twig_Environment */
+		$twig = $app['twig'];
 
-	return $twig->render('index.twig', array('mystring' => 'Test string'));
-});
+		return $twig->render('index.twig', array('mystring' => 'Test string'));
+	}
+);
 
 return $app;
